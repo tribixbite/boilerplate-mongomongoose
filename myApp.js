7478a4b613760c66f73305deeb206e32335b167f
Will <willstone@gmail.com>
2021-03-13 03:52:26 +0000

    findById added to findPersonById

--- myApp.js
+++ myApp.js
@@ -85,7 +85,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, data){
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
